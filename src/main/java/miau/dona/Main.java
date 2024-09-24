24e73c5e67bbddbc34476030b8dda26706e9588c
miaudona <donanferyt@gmail.com>
2024-09-24 23:44:32 +0200

    gsdfgd

---
+++ src/main/java/miau/dona/Main.java
@@ -0,0 +1,24 @@
+package miau.dona;
+
+import java.util.Objects;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+            double precio;
+            int people;
+            String day;
+            boolean isStudent;
+
+            Scanner scanner = new Scanner(System.in);
+            day = scanner.nextLine();
+
+            if (Objects.equals(day, "Wednesday")) {
+                precio = 5;
+
+            } else if {
+
+        }
+    }
+}
