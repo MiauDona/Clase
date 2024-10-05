13e57d504687ee213fb418003dc4ae76b7d67bd1
miaudona <donanferyt@gmail.com>
2024-10-05 02:57:11 +0200

    Clean code

--- src/main/java/miau/dona/tareas/EjerciciosDificil.java
+++ src/main/java/miau/dona/tareas/EjerciciosDificil.java
@@ -1,6 +1,5 @@
 package miau.dona.tareas;
 
-import jdk.jshell.execution.Util;
 import miau.dona.utils.UtilsJava;
 
 public class EjerciciosDificil {
@@ -51,80 +50,34 @@ public class EjerciciosDificil {
 
     public static void numeroPerfectoRango() {;
         int max, min;
-        min = 1;
-        max = 28;
+        min = UtilsJava.pedirInt("Dime un numero minimo");
+        max = UtilsJava.pedirInt("Dime un numero maximo");
         for (int i = min; i <= max; i++) { // i es la "n" de la formula del numero perfecto (NumPrimo)
             if (i == 1) {
                 continue;
             }
-            esNumeroPerfecto(i);
-
-        }
-
-
-    }
-    public static void crearNumeroPerfecto(int numeroIntroducido) {
-        int numPrimo = numeroIntroducido; // Numero de veces que se ejecuta el programa tambien, debe de ser primo
-        int formulaNumPrimo = (int) Math.pow(2, numPrimo) - 1;
-        int formulaUltimoNumero = (int) Math.pow(2, numPrimo - 1);
-        int formulaPerfecto = formulaNumPrimo * formulaUltimoNumero;
-        int var = 0;
-        int num = 1;
-        for (int i= 0; i < numPrimo; i++) {
-            var = var + num;
-            num = num * 2;
-            // System.out.println("Num " + num + " Var " + var);
-        }
-
-        // System.out.println("Ultimo numero a partir de la formula " + formulaUltimoNumero);
-        System.out.println("Numero perfecto " + formulaPerfecto);
-        // System.out.println("Numero primo con la formula " + formulaNumPrimo);
-
-        /*int ultimoNumero = var;
-        if (ultimoNumero == formulaNumPrimo) {
-            System.out.println("Es un numero de Menesser o cmo se llame");
-        }*/
-    }
-
-    public static boolean esNumeroPrimo(int numeroIntroducido) {
-        boolean esPrimo = true;
-        for (int i = numeroIntroducido-1; i > 1; i--) {
-            if (numeroIntroducido % i == 0) {
-                esPrimo = false;
-                System.out.println("El número no es primo");
-                break;
+            if (esNumeroPerfecto(i)) {
+                System.out.println("El numero de perfecto es: " + i);
             }
         }
-
-        return esPrimo;
-
     }
 
-    public static void esNumeroPerfecto(int numeroIntroducido) {
+    public static boolean esNumeroPerfecto(int numeroIntroducido) {
         int numAComprobar = numeroIntroducido; // Numero de veces que se ejecuta el programa tambien, debe de ser primo
         int n = 0;
         int formulaPerfecto;
         for (int i = 1; i < numeroIntroducido; i++) {
-            n=i;
-            formulaPerfecto = (int) ((Math.pow(2,n) - 1) * Math.pow(2,n-1));
+            n = i;
+            formulaPerfecto = (int) ((Math.pow(2, n) - 1) * Math.pow(2, n - 1));
             if (formulaPerfecto == numAComprobar) {
                 System.out.println("Es un numero perfecto. NumAcomprobar " + numAComprobar + " resultadoFormula " + formulaPerfecto);
+                return true;
             }
         }
-
-        // ultimonum * numprimosumaserie = numeroperfecto
-        // 6 = ultimonum * numPrimoSumaSerie
-        // n=x;
-        // 6 = (2^x - 1) * ((2^x) - 1)
-
-
-        //System.out.println("Numeros " + numeroIntroducido + " " + formulaPerfecto);
-       /* if (numeroIntroducido == formulaPerfecto ) {
-            System.out.println("Es igual");
-        }*/
-
-
+        return false;
     }
+
+
 
 
 
@@ -135,5 +88,4 @@ public class EjerciciosDificil {
     */
 
 
-
-}
+    }
--- src/main/java/miau/dona/utils/UtilsJava.java
+++ src/main/java/miau/dona/utils/UtilsJava.java
@@ -3,34 +3,42 @@ package miau.dona.utils;
 import java.util.Scanner;
 
 public class UtilsJava {
-    public static int pedirInt(String mensaje) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println(mensaje);
-        return scanner.nextInt();
-    }
-    public static String pedirString(String mensaje) {
+    public static String pedir(String mensaje) {
         Scanner scanner = new Scanner(System.in);
         System.out.println(mensaje);
         return scanner.nextLine();
     }
-    public static double pedirDouble(String mensaje) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println(mensaje);
-        return scanner.nextDouble();
+
+    public static int pedirInt(String mensaje) {
+        return Integer.parseInt(pedir(mensaje));
     }
+
+    public static double pedirDouble(String mensaje) {
+        return Double.parseDouble(pedir(mensaje));
+    }
+
     public static float pedirFloat(String mensaje) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println(mensaje);
-        return scanner.nextFloat();
+        return Float.parseFloat(pedir(mensaje));
     }
 
     public static double pedirNumerosParaRandom() {
-        Scanner scanner = new Scanner(System.in);
-
         int min = pedirInt("Vamos a hacer un número aleatorio en un rango entre 2 numeros. Dime el número mínimo");
         int max = pedirInt("Dime el número máximo");
-        double numRandom = Math.random() * (max - min) + min ;
-        return numRandom;
+
+        return Math.random() * (max - min) + min;
     }
 
+    public static boolean esNumeroPrimo(int numeroIntroducido) {
+        boolean esPrimo = true;
+
+        for (int i = numeroIntroducido-1; i > 1; i--) {
+            if (numeroIntroducido % i == 0) {
+                esPrimo = false;
+                System.out.println("El número no es primo");
+                break;
+            }
+        }
+
+        return esPrimo;
+    }
 }
