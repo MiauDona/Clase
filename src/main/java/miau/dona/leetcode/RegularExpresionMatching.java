ea3afe14cccd7bab690c15c55c6517511c2fd911
miaudona <donanferyt@gmail.com>
2024-10-10 00:47:28 +0200

    NO entiendo nada ayuda

--- src/main/java/miau/dona/leetcode/RegularExpresionMatching.java
+++ src/main/java/miau/dona/leetcode/RegularExpresionMatching.java
@@ -2,36 +2,72 @@ package miau.dona.leetcode;
 
 import miau.dona.utils.UtilsJava;
 
-import java.util.Arrays;
-
 public class RegularExpresionMatching {
     public static void main(String[] args) {
-        String miString = "aaaaaaa";
-        String miPatron = "aaaaaaa";
+        String miString = "aaaaaa";
+        String miPatron = "*aaaa.";
         System.out.println(isMatch(miString, miPatron));
 
         // TODO Falta hacer que cuando miString sea mayor que miPatron pero miPatron tenga * al final, sea todo lo anterior true
     }
 
     public static boolean isMatch(String s, String p) {
+        boolean coincide = false;
 
-        boolean[] booleans = new boolean[p.length()];
         char[] pArray = p.toCharArray();
         char[] sArray = s.toCharArray();
 
-
         // TODO: Falta hacer que si una letra de pArray es distinta de sArray que sea false
-        if (pArray[pArray.length-1] == '*' && sArray.length >= pArray.length) {
-            for (int i = 0; i < pArray.length-1; i++) {
-                if (pArray[i] != sArray[i]) {
-                    System.out.println("***");
+
+        // Si la String es mas pequeña que el patron
+        if (sArray.length < pArray.length) {
+            boolean[] booleans = new boolean[s.length()];
+            for (int i = 0; i < sArray.length; i++) {
+                if (sArray[i] == pArray[i] || pArray[i] == '.' || pArray[i] == '*') {
+                    booleans[i] = true;
+                    coincide = true;
+                } else {
                     return false;
+                }
+            }
+
+            for (boolean b : booleans) {
+                if (b) {
+                    coincide = true;
+                }
+            }
+
+            for (int i = sArray.length; i < pArray.length; i++) {
+                if (pArray[i] != '*') {
+                    return false;
+                } else {
+                    coincide = true;
                 }
             }
         }
 
-        if (pArray.length == sArray.length) {
+        // Si la String es mas grande que el patron
+        // Este funciona
+        if (pArray[pArray.length-1] == '*' && sArray.length > pArray.length) {
             for (int i = 0; i < pArray.length; i++) {
+                if (pArray[i] == sArray[i] || pArray[i] == '*' || pArray[i] == '.') {
+                    coincide = true;
+                } else {
+                    return false;
+                }
+                if (i <= sArray.length-1 && (pArray[i] == '*')) {
+                    coincide = true;
+                } else {
+                    coincide = false;
+                }
+            }
+        }
+
+        // Si los 2 son iguales
+        if (pArray.length == sArray.length) {
+            boolean[] booleans = new boolean[p.length()];
+            for (int i = 0; i < pArray.length; i++) {
+
                 if (pArray[i] == sArray[i] || pArray[i] == '.' || pArray[i] == '*') {
                     booleans[i] = true;
                 } else {
@@ -43,9 +79,8 @@ public class RegularExpresionMatching {
                     return true;
                 }
             }
-            UtilsJava.mostrarArrayBooleans(booleans);
         }
-        return false;
+        return coincide;
     }
 
     /*
